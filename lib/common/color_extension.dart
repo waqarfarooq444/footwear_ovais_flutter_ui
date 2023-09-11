0a4b8200f07a2b4130dae4a2f23349524f6c7a12
Muhammad Waqar <waqarfarooq444@icloud.com>
2023-09-11 12:34:55 +0500

    flutter_ui_sept_project

---
+++ lib/common/color_extension.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TColor {
+  static Color get primary => const Color(0xff153075);
+  static Color get primaryText => const Color(0xff2A4BA0);
+  static Color get secondaryText => const Color(0xff606D76);
+  static Color get textfield => const Color(0xffF2F2F2);
+  static Color get placeholder => const Color(0xffB6B7B7);
+  static Color get white => const Color(0xffffffff);
+}
